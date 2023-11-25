94f21d2946a96e7266f455a1616eaae2f690bd32
Shawn Arlantico <arlantico.shawn@gmail.com>
2023-11-24 16:21:40 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import TicTacToe from "./routes/TicTacToe";
 import GameData from "./routes/DataApi";
-import Navbar from "./Navbar";
 
 export default function App() {
   return (
     <>
-    <Navbar />
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
