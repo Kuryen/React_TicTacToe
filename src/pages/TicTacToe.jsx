f09be02a1cfc60bdc66710cf6b8dbf0d3c1e1771
Shawn Arlantico <arlantico.shawn@gmail.com>
2023-11-24 16:01:42 -0800

    added Navbar to tictactoe page

--- src/pages/TicTacToe.jsx
+++ src/pages/TicTacToe.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Navbar from "../components/Navbar";
 
 function Square({ value, onSquareClick }) {
   return (
@@ -10,9 +11,10 @@ function Square({ value, onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
+
     const nextSquares = squares.slice();
     if (xIsNext) {
       nextSquares[i] = "X";
@@ -55,7 +57,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
+  const xIsNext = currentMove % 2 == 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
@@ -70,27 +72,35 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+
     if (move > 0) {
       description = "Go to move #" + move;
     } else {
       description = "Go to game start";
     }
+
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
   });
-
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <>
+      <Navbar />
+      <div className="game">
+        <div className="game-board">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <ol>{moves}</ol>
-      </div>
-    </div>
+    </>
   );
 }
 
@@ -105,9 +115,10 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+    if (squares[i] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
